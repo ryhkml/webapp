59fc3775fb8feefcd41414c049750886552d0f1e
ryhkmll <reyhank95@hotmail.com>
2022-08-29 10:32:52 +0700

    build: init app

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Build stage
+FROM ekidd/rust-musl-builder AS build
+
+WORKDIR /app
+
+COPY src ./src/
+COPY www ./www/
+COPY Cargo* ./
+
+RUN cargo build --release
+
+# Final stage
+FROM scratch
+
+LABEL maintainer="Reyhan Kamil <reyhank95@hotmail.com>"
+
+ENV TZ=Asia/Jakarta
+ENV PORT=22333
+
+WORKDIR /bin
+
+COPY --from=build /app/target/x86_64-unknown-linux-musl/release/rey_webapp ./
+COPY --from=build /app/www ./www/
+
+EXPOSE ${PORT}
+
+CMD ["/bin/rey_webapp"]
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+extern crate dotenv;
+
+use std::env;
+
+use warp::Filter;
+use warp::hyper::Method;
+use warp::http::header::HeaderMap;
+use dotenv::dotenv;
+
+#[tokio::main]
+async fn main() {
+
+    dotenv().ok();
+
+    let mut headers = HeaderMap::new();
+    headers.insert("X-Frame-Options", "DENY".parse().unwrap());
+    headers.insert("X-Content-Type-Options", "nosniff".parse().unwrap());
+    headers.insert("Strict-Transport-Security", "max-age=31536000; includeSubDomains".parse().unwrap());
+    headers.insert("X-XSS-Protection", "1; mode=block".parse().unwrap());
+
+    let port = env::var("PORT")
+    .unwrap()
+    .parse::<u16>()
+    .unwrap();
+
+    let routes = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::dir("www"))
+        .with(warp::cors().allow_any_origin().allow_methods(&[Method::GET, Method::OPTIONS]))
+        .with(warp::reply::with::headers(headers));
+
+    warp::serve(routes)
+        .run(([0, 0, 0, 0], port))
+        .await;
+}
