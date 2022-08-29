2431074095f077611bc6d27c176c5df31505a173
ryhkmll <reyhank95@hotmail.com>
2022-08-29 14:20:46 +0700

    build: add new robots.txt & sitemap.xml

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ async fn main() {
         .unwrap();
 
     let routes = warp::get()
-        .and(warp::path::end())
         .and(warp::fs::dir("www"))
+        .and(warp::path::end())
         .with(warp::cors().allow_any_origin().allow_methods(&[Method::GET, Method::OPTIONS]))
         .with(warp::reply::with::headers(headers));
     
